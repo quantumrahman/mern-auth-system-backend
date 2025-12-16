03ddc31caeb59db10089924b3933106a89d7ab97
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-12-16 19:58:57 +0600

    create generate.otp.func.js file and implement generateOTP function in this file

---
+++ src/utils/generator/generate.otp.func.js
@@ -0,0 +1,11 @@
+// import modules --------------------------------------------->
+import crypto from 'crypto';
+
+// otp func --------------------------------------------------->
+const generateOTP = (min = 100000, max = 1000000) => {
+    const six_digit_otp = crypto.randomInt(min, max);
+    return six_digit_otp;
+};
+
+// export modules --------------------------------------------->
+export default generateOTP;
