156247455ac6dd6bf71345b419d6c268a01beaa1
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-12-16 19:26:04 +0600

    create express app in app.js file

---
+++ src/app.js
@@ -0,0 +1,8 @@
+// import modules --------------------------------------------->
+import express from "express";
+
+// create app ------------------------------------------------->
+const app = express();
+
+// export modules --------------------------------------------->
+export default app;
