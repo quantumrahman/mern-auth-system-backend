6a12b80eb58b67896c75e959915562db3d860e95
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-12-17 14:36:27 +0600

    change code in auth routes file get to post

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -6,7 +6,7 @@ import authSignupController from '../controllers/auth.signup.controller.js';
 const router = express.Router();
 
 // declare route ---------------------------------------------->
-router.route('/sign_up').get(authSignupController);
+router.route('/sign_up').post(authSignupController);
 router.route('/sign_in');
 router.route('/sign_out');
 
