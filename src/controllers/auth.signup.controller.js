7cadd9e2265f22a0838f191cd285c2c9405853cb
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-12-17 12:55:54 +0600

    create auth.signup.controller.js file and implement sign up controlling function

---
+++ src/controllers/auth.signup.controller.js
@@ -0,0 +1,9 @@
+// import modules --------------------------------------------->
+
+// signup controller ------------------------------------------>
+const signup_controller = async (req, res) => {
+    res.send('sign up controller');
+};
+
+// export modules --------------------------------------------->
+export default signup_controller;
