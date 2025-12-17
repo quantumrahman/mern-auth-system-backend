084ff92574170b1ad8c4d10e147f16cf9e05d3ae
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-12-17 12:59:03 +0600

    add auth sign up controller in auth.routes.js file

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,12 @@
 // import modules --------------------------------------------->
 import express from 'express';
+import authSignupController from '../controllers/auth.signup.controller.js';
 
 // route ------------------------------------------------------>
 const router = express.Router();
 
 // declare route ---------------------------------------------->
-router.route('/sign_up');
+router.route('/sign_up').get(authSignupController);
 router.route('/sign_in');
 router.route('/sign_out');
 
