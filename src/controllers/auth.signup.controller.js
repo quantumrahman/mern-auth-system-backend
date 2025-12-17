8b527a2e6d0b0108119e799652970e045cae5af1
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-12-17 14:34:56 +0600

    import some necessary modules in auth.signup.controller.js file

--- src/controllers/auth.signup.controller.js
+++ src/controllers/auth.signup.controller.js
@@ -1,4 +1,9 @@
 // import modules --------------------------------------------->
+import User from '../models/auth.model.js';
+import AppError from '../utils/constant/app.error.js';
+import generateOtp from '../utils/generator/generate.otp.func.js';
+import generateJwt from '../utils/generator/generate.jwt.func.js';
+import generateHash from '../utils/generator/generate.hash.func.js';
 
 // signup controller ------------------------------------------>
 const signup_controller = async (req, res) => {
